a151a7c40bf1ae2f8bd83bf277e8b9faaebe271a
Jonas Maurus <jonas-github@maurus.net>
2018-02-07 17:58:39 +0100

    bump for release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except IOError:
 
 setup(
     name="12factor-vault",
-    version="0.1.19",
+    version="0.1.20",
     packages=["vault12factor"],
     classifiers=[
         "Development Status :: 4 - Beta",
